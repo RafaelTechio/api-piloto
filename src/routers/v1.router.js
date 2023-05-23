d3f76a19f0a798259dac54294cb7e144b9247aeb
Yuri-Toledo <110365951+Yuri-Toledo@users.noreply.github.com>
2023-05-23 15:52:41 -0300

    feat: Adicionando crud de esp

--- src/routers/v1.router.js
+++ src/routers/v1.router.js
@@ -2,9 +2,11 @@ require('express-async-errors');
 const { Router } = require('express');
 
 const MainteinerRouter = require('./mainteiner.router');
+const EspRouter = require('./esp.router');
 
 const router = Router();
 
 router.use('/mainteiners/', MainteinerRouter);
+router.use('/esps/', EspRouter);
 
 module.exports = router;
